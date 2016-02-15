6ce48a844b38d7b0122be2153a15c3394efe78cb
Alberto <simioni.alberto@gmail.com>
2016-02-15 14:53:30 +0100

    Added comments to reorg2

--- reorg2.c
+++ reorg2.c
@@ -33,7 +33,7 @@ void save_locations()
     short location;
 
     location_map = malloc(sizeof(short) * (person_length / sizeof(Person)));
-    loc_size = LOCATION_BUFFER; //we set a variable size to the array of locations 
+    loc_size = LOCATION_BUFFER; //we set a variable size to the array of locations
     visited_locations = malloc(sizeof(short) * loc_size);
 
     //runs through the person_map and add a location to the visited_locations array each time a new one is found
@@ -111,7 +111,7 @@ void reorg_location(char* path, char *knows_output_file, char * person_output_fi
 
         person_copy.knows_n = count;
         fwrite(&person_copy, sizeof(Person), 1, person_file);
-       
+
     }
     fclose(person_file);
     fclose(knows_file);
@@ -120,7 +120,7 @@ void reorg_location(char* path, char *knows_output_file, char * person_output_fi
     //open the correct person and knows map
     person_map   = (Person *)       mmapr(makepath(path, person_output_file,   "bin"), &person_length);
     knows_map    = (unsigned int *) mmapr(makepath(path, knows_output_file,    "bin"), &knows_length);
-
+    free(location_map);
     return;
 }
 
@@ -134,7 +134,7 @@ void reorg_location_mutual(char *path, char *knows_output_file, char *person_out
     unsigned short count;
     unsigned int person_count=0;
     unsigned long total_count = 0;
-    //buffer containing the new offsets of person_map 
+    //buffer containing the new offsets of person_map
     unsigned int *offests_buffer=malloc(sizeof(int) *person_length / sizeof(Person));
 
     Person *person, *knows;
@@ -184,7 +184,7 @@ void reorg_location_mutual(char *path, char *knows_output_file, char *person_out
     }
     fclose(knows_file);
     fclose(person_file);
-
+    munmap(knows_map,knows_length);
     knows_map_reduced    = (unsigned int *)   mmapr(makepath(path, "knows_temp",    "bin"), &knows_length);
     knows_file = open_binout(makepath(path, knows_output_file, "bin"));
     //substitute in the knows file the updated offsets
@@ -193,45 +193,51 @@ void reorg_location_mutual(char *path, char *knows_output_file, char *person_out
         fwrite(&offests_buffer[knows_map_reduced[knows_offset]], sizeof(int), 1, knows_file);
     }
     munmap(person_map, person_length);
+    munmap(knows_map_reduced,knows_length);
     person_map = (Person *) mmapr(makepath(path, person_output_file, "bin"), &person_length);
 
     return;
 }
 
-
+//function that inverts the relation between interests and person. It creates a table with all the different artists and a table
+//that contains all the person offsets of the people that likes those artists
 void reorg_interests(char *path, char* liked_file, char* artists_file)
 {
-    //CODE FOR CREATING THE likedBy table and to create the new interests table
+    //output files for the two new tables
     FILE *artists = open_binout(makepath(path, artists_file, "bin"));
     FILE *likedBy = open_binout(makepath(path, liked_file, "bin"));
-    unsigned int interests_buffer_size = INTEREST_BUFFER;
-    unsigned int interests_count = 0;
-    unsigned long total_count = 0;
-    unsigned short* interests_buffer  = malloc(sizeof(short) * interests_buffer_size);
-    unsigned long interest_offset, interest_buffer_offset;
-    unsigned short current_interest, person_interest;
-    unsigned short found;
+
+    unsigned int interests_buffer_size = INTEREST_BUFFER; //size of the interest_buffer
+    unsigned int interests_count = 0; //number of different interests founded
+    unsigned short* interests_buffer  = malloc(sizeof(short) * interests_buffer_size); //buffer that contains all the different interests
+    unsigned long interest_offset;
+    unsigned short current_interest;
+    unsigned short found; //boolean that indicates if the current interest is already inside the interest_buffer
     unsigned int person_offset;
     Person *person;
-    //Populating the array of different interests
 
+    //First phase: filling the interests_buffer with all the different artists. The computation goes through all the different person
+    //and their interests and each time a new interest is found, it's added to the interest_buffer.
+    //If an artists isn't liked by any person, it won't be added to the buffer
     for (person_offset = 0; person_offset < person_length / sizeof(Person); person_offset++)
     {
-
         person = &person_map[person_offset];
+        //iterates all the interests of a person
         for (interest_offset = person->interests_first;
                 interest_offset < person->interests_first + person->interest_n;
-                interest_offset++){
+                interest_offset++)
+        {
             found = 0;
             current_interest = interest_map[interest_offset];
             unsigned int i;
-            for(i = 0; i < interests_count && !found; i++){
-                if(current_interest == interests_buffer[i]){
+            for(i = 0; i < interests_count && !found; i++)
+            {
+                if(current_interest == interests_buffer[i]){ //true: interest already present
                     found = 1;
                 }
             }
 
-            if(!found)
+            if(!found) //founded new interest
                 {
                 if(interests_count == interests_buffer_size)
                     {
@@ -239,60 +245,79 @@ void reorg_interests(char *path, char* liked_file, char* artists_file)
                         interests_buffer_size = interests_buffer_size * 2;
                         interests_buffer = realloc(interests_buffer, interests_buffer_size * sizeof(short));
                     }
-                interests_buffer[interests_count] = current_interest;
+                interests_buffer[interests_count] = current_interest; //new interest added in the buffer
                 interests_count++;
                 }
-            }
+        }
     }
 
-    //for(interest_offset = 0; interest_offset < interests_count; interest_offset++){ printf("%hi\n",interests_buffer[interest_offset] );}
-    printf("%li \n", person_length / sizeof(Person));
-    printf("interests: %i\n",interests_count);
 
-
-    unsigned int artist_start;
-    unsigned int artist_end = 0;
-    unsigned int artist_counts[ARTIST_BUFFER_NUMBER];
-    unsigned int likedBy_buffer_sizes[ARTIST_BUFFER_NUMBER];
+    //Second phase: it computes the likedBy table. It finds all the people that like the interests inside the buffer
+    //To compute it without using too much memory, this phase is divided in multiple equal steps. We compute in each step
+    //the likedBy table of ARTIST_BUFFER_NUMBER different artists(interests). In each step only a part of the table is computed
+    unsigned short person_interest;
+    unsigned int artist_start; //First artist considered in the current step
+    unsigned int artist_end = 0; //Number of the last artist considered in the current step
+    unsigned int artist_counts[ARTIST_BUFFER_NUMBER]; //array that contains in each entry the number of people that like each artist
+    //buffers that contain the offset of the people that like each artist. One buffer per artist
     unsigned int *likedBy_buffer[ARTIST_BUFFER_NUMBER];
-    unsigned long total_count2 = 0;
+    //sizes of the buffers
+    unsigned int likedBy_buffer_sizes[ARTIST_BUFFER_NUMBER];
+    //Counter of the number of likes (counter of the current dimension the likedBy table)
+    unsigned long total_count = 0;
     unsigned int initial_likedBy_buffer_size = 20;
-    int i;
-    for(i = 0; i < ARTIST_BUFFER_NUMBER; i++){
-            likedBy_buffer_sizes[i] = initial_likedBy_buffer_size;
-        }
-    for(i = 0;i < ARTIST_BUFFER_NUMBER; i++){
-            likedBy_buffer[i] = malloc(sizeof(int) * likedBy_buffer_sizes[i]);
-        }
 
+    //buffer initialization
+    int i;
+    for(i = 0; i < ARTIST_BUFFER_NUMBER; i++)
+    {
+            likedBy_buffer_sizes[i] = initial_likedBy_buffer_size;
+    }
+    for(i = 0;i < ARTIST_BUFFER_NUMBER; i++)
+    {
+            likedBy_buffer[i] = malloc(sizeof(int) * likedBy_buffer_sizes[i]);
+    }
+
+    //Stop = 1 when we considered all the artists in different steps
     int stop = 0;
-    while(!stop){
-        artist_counts[4] = 5;
+    while(!stop)
+    {
+        //Clearing the counters
         bzero(artist_counts, sizeof(int) * ARTIST_BUFFER_NUMBER);
-        stop = 0;
+
+        //Moving the indexes of the current artists considered
         artist_start = artist_end;
-        if(artist_end + ARTIST_BUFFER_NUMBER >= interests_count){
+        if(artist_end + ARTIST_BUFFER_NUMBER >= interests_count)
+        {
             artist_end = interests_count;
             stop = 1;
         }
         else{
             artist_end += ARTIST_BUFFER_NUMBER;
         }
-        printf("START = %i , END = %i\n",artist_start,artist_end );
+        //Arrays of artists that represent the current artists considered
         Artist artists_buffer[ARTIST_BUFFER_NUMBER];
+
+        //We go through all the person to search their interests
         for (person_offset = 0; person_offset < person_length / sizeof(Person); person_offset++)
         {
             person = &person_map[person_offset];
+            //interest of the current person
             for (interest_offset = person->interests_first;
                 interest_offset < person->interests_first + person->interest_n;
                 interest_offset++){
+
                 person_interest =  interest_map[interest_offset];
+
+                //Searching the person_interest inside the interests used in this step
                 found = 0;
                 int j;
-                for(j = 0; j < ARTIST_BUFFER_NUMBER && !found; j++){
-                    if(person_interest == interests_buffer[artist_start+j]){
+                for(j = 0; j < ARTIST_BUFFER_NUMBER && !found; j++)
+                {
+                    //true if the person_interest is in the current step
+                    if(person_interest == interests_buffer[artist_start+j])
+                    {
                         found = 1;
-
                         if(artist_counts[j] == likedBy_buffer_sizes[j])
                             {
                                 //printf("doubling size of likedBy %i\n", j);
@@ -300,22 +325,28 @@ void reorg_interests(char *path, char* liked_file, char* artists_file)
                                 likedBy_buffer[j] = realloc(likedBy_buffer[j],sizeof(int) * likedBy_buffer_sizes[j]);
                             }
 
-                        likedBy_buffer[j][artist_counts[j]] = person_offset;
-                        artist_counts[j]++;
+                        likedBy_buffer[j][artist_counts[j]] = person_offset; //adding the person to the buffer of the artist
+                        artist_counts[j]++; //updating the counter of the artist
                         }
                     }
                 }
             }
-            //ALLA FINE AGGIORNO TUTTI I CAMPI DEGLI ARTIST E LI BUTTO GIU SU DISCO, AGGIORNO ANCHE IL TOTAL_COUNT
-            for(i = 0;i < ARTIST_BUFFER_NUMBER; i++){
+            //Writing to file the artists considered in the step to disk and writing to the likedBy file their likedBy_buffer
+            for(i = 0;i < ARTIST_BUFFER_NUMBER; i++)
+            {
                 fwrite(likedBy_buffer[i], sizeof(int), artist_counts[i], likedBy);
-                artists_buffer[i].likedBy_first = total_count2;
-                total_count2 += artist_counts[i];
+                artists_buffer[i].likedBy_first = total_count;
+                total_count += artist_counts[i]; //updating the count of the likedBy after writing to file
                 artists_buffer[i].interest_id = interests_buffer[artist_start+i];
                 artists_buffer[i].likedBy_n = artist_counts[i];
                 fwrite(&artists_buffer[i], sizeof(Artist), 1, artists);
             }
         }
+    //deallocation of the buffers
+    for(i = 0;i < ARTIST_BUFFER_NUMBER; i++){
+        free(likedBy_buffer[i]);
+    }
+    free(interests_buffer);
 
     fclose(artists);
     fclose(likedBy);
@@ -348,9 +379,9 @@ void reorg_person(char *path, char *person_file){
 
 //order the person by birthday and write them in a new file
 void reorg_person_birthday(char *path, char *birthday_file){
-    
+
     FILE *person_birthday_file = open_binout(makepath(path, birthday_file, "bin"));
-    
+
     Person *person;
     unsigned int person_offset;
     Person_birthday person_copy;
@@ -367,6 +398,7 @@ void reorg_person_birthday(char *path, char *birthday_file){
     qsort(birthdays, person_length / sizeof(Person), sizeof(Person_birthday), &person_birthday_comparator);
 
     fwrite(birthdays,person_offset,sizeof(Person_birthday),person_birthday_file);
+    free(birthdays);
     fclose(person_birthday_file);
 }
 
@@ -384,11 +416,12 @@ int main(int argc, char *argv[])
     reorg_location_mutual(argv[1], "knows_mutual", "person_mutual");
 
     reorg_interests(argv[1], "likedBy", "artists");
-    
+
     reorg_person_birthday(argv[1], "birthday");
 
     reorg_person(argv[1], "person_compact_reduced");
 
-
+    munmap(person_map,person_length);
+    munmap(interest_map,interest_length);
     return 0;
 }
