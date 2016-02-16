5bac16653965354e29ddcff0ffbc94d2c3a6cab1
Alberto <simioni.alberto@gmail.com>
2016-02-16 14:24:23 +0100

    Simple commented

--- simple.c
+++ simple.c
@@ -20,17 +20,19 @@
 Person_compact *person_map;
 Person_birthday *person_birthday_map;
 unsigned int *knows_map;
-unsigned char *scores_map;
-char *path;
+unsigned char *scores_map; //This map isn't a mmap file but is an array populated in the precomputation
+char *path; //Path to the folder with the data
 Artist *artists_map;
 unsigned int *liked_map;
-clock_t start, end;
 
 
 unsigned long person_length, knows_length, score_length, artists_length, liked_length, person_birthday_length;
 
 FILE *outfile;
 
+//Binary search in the person_birthday_map, the map is ordered with the smallest birthday at the sttart of the array
+//In the map there can be multiple entrys with the same birthday value, the function return always the index of the
+//first element with that value.
 int binsearch(int start, int end, short value)
 {
     int middle;
@@ -49,6 +51,8 @@ int binsearch(int start, int end, short value)
         }
         else
         {
+            //value founded, we now search for the first element with the same value in the map
+            //by reducing the middle index
             while(middle >= start && person_birthday_map[middle].birthday == value)
             {
                 --middle;
@@ -87,23 +91,29 @@ int result_comparator(const void *v1, const void *v2)
         return 0;
 }
 
+//Precomputes the scores values storing them inside the scores_map. The function uses the artist_map and the likedBy_map
+//That are created in the reorg phase.
 void save_scores(unsigned short artist_id, unsigned short areltd[])
 {
-    unsigned int artist_offset;
-    unsigned long liked_offset;
+    unsigned int artist_offset; //offset in the artist_map
+    unsigned long liked_offset; //offset in the likedBy_map
     Artist *artist;
 
+    //for each person there is an entry with his score
+    //if the score = 4, the person likes A1 (artist_id)
     scores_map = malloc(sizeof(char) * (person_length / sizeof(Person_compact)));
     bzero(scores_map, sizeof(char) * (person_length / sizeof(Person_compact)));
 
+    //iterating through the artist_map only one time
     for (artist_offset = 0;
             artist_offset < artists_length / sizeof(Artist);
             artist_offset++)
     {
 
         artist = &artists_map[artist_offset];
-        if(artist->interest_id == artist_id)
+        if(artist->interest_id == artist_id) //true: current artist = A1
         {
+            //Assigns the value 4 to all the people that likes A1
             for(liked_offset = artist->likedBy_first;
                     liked_offset < artist->likedBy_first + artist->likedBy_n;
                     liked_offset++)
@@ -111,8 +121,11 @@ void save_scores(unsigned short artist_id, unsigned short areltd[])
                 scores_map[liked_map[liked_offset]] = 4;
             }
         }
+
+        //true: current artist = A2 or A3 or A4
         if(areltd[0] == artist->interest_id || areltd[1] == artist->interest_id || areltd[2] == artist->interest_id)
         {
+            //Increament the score by 1 to all the people that likes the artist
             for(liked_offset = artist->likedBy_first;
                     liked_offset < artist->likedBy_first + artist->likedBy_n;
                     liked_offset++)
@@ -140,20 +153,17 @@ void query(unsigned short qid, unsigned short artist, unsigned short areltd[], u
     pbday.birthday = bdstart;
     int person_num = person_birthday_length / sizeof(Person_birthday);
 
-    int start_pos = binsearch(0, person_num, bdstart);
-    //int start_pos=0;
+    int start_pos = binsearch(0, person_num, bdstart); //retrieving the first person that is inside the bdstart,bdend interval
+
     for(offset = start_pos; offset < person_num && person_birthday_map[offset].birthday <= bdend; offset++)
     {
-        //if(person_birthday_map[offset].birthday<bdstart) continue;
-        
-        // but person must like some of these other guys
+
         score = scores_map[person_birthday_map[offset].person_offset];
-        if (score == 0 || score == 4) continue;
+        if (score == 0 || score == 4) continue; //score = 4 means that person likes A1
 
         person = &person_map[person_birthday_map[offset].person_offset];
 
-
-        // check if friend lives in same city and likes artist
+        //iterating mutual friendships
         for (knows_offset = person->knows_first;
                 knows_offset < person->knows_first + person->knows_n;
                 knows_offset++)
@@ -161,7 +171,7 @@ void query(unsigned short qid, unsigned short artist, unsigned short areltd[], u
             knows_pos = knows_map[knows_offset];
             knows = &person_map[knows_pos];
 
-            // check if friend likes artist
+            // check if friend likes A1
             if (scores_map[knows_pos] != 4) continue;
             // realloc result array if we run out of space
             if (result_length >= result_set_size)
@@ -202,45 +212,43 @@ void query_line_handler(unsigned char nfields, char **tokens)
     q_bdaystart     = birthday_to_short(tokens[QUERY_FIELD_BS]);
     q_bdayend       = birthday_to_short(tokens[QUERY_FIELD_BE]);
 
+    //This two maps are used only in the precomputation, they are opened here to be able to close them after the precomputation
     artists_map = (Artist *)  mmapr(makepath(path, "artists",    "bin"), &artists_length);
-
     liked_map = (unsigned int *)  mmapr(makepath(path, "likedBy",    "bin"), &liked_length);
 
-     //start = clock();
     save_scores(q_artist, q_relartists);
-    //end = clock();
-    //printf("Scores: %i\n", end-start);
-munmap(artists_map, artists_length);
-munmap(liked_map, liked_length);
-    //start = clock();
+
+    //Closing the maps
+    munmap(artists_map, artists_length);
+    munmap(liked_map, liked_length);
+
     query(q_id, q_artist, q_relartists, q_bdaystart, q_bdayend);
-     //end = clock();
-    //printf("query:  %i\n", end-start);
+
 }
 
 
 
 int main(int argc, char *argv[])
 {
-    //start=clock();
+
     if (argc < 4)
     {
         fprintf(stderr, "Usage: [datadir] [query file] [results file]\n");
         exit(1);
     }
     path=argv[1];
-    /* memory-map files created by loader */
+
     person_map   = (Person_compact *)    mmapr(makepath(argv[1], "person_compact",   "bin"), &person_length);
     knows_map    = (unsigned int *)   mmapr(makepath(argv[1], "knows_mutual",    "bin"), &knows_length);
     person_birthday_map = (Person_birthday *) mmapr(makepath(argv[1], "birthday", "bin"), &person_birthday_length);
+
     outfile = fopen(argv[3], "w");
     if (outfile == NULL)
     {
         fprintf(stderr, "Can't write to output file at %s\n", argv[3]);
         exit(-1);
     }
+
     parse_csv(argv[2], &query_line_handler);
- //end = clock();
-    //printf("Main:  %i\n", end-start);
     return 0;
 }
